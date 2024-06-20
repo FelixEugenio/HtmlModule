abb9e42955a293d831b52a31d05662a5634e1b85
felix mavila <felixmacarenco@gmail.com>
2024-06-20 01:45:10 +0100

    chore: javascript module arrays

---
+++ javascript/arrays.js
@@ -0,0 +1,19 @@
+const  numeros = [1,2,3,4,5,6,7,8,9];
+[primeiroNumero,segundoNumero,...rest] = numeros;
+
+/*
+function somar(n1,n2){
+    return resultado = n1+n2;
+}
+    */
+
+const matriz = [[1,2,3],[4,5,6],[7,8,9]]
+
+const [lista1,lista2,lista3] = matriz;
+
+console.log(lista1[2])
+
+//console.log(somar(primeiroNumero,rest))
+
+
+
