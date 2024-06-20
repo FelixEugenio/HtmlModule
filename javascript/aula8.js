632edd17d6c542d75b47057076b8e421a9731dfe
felix mavila <felixmacarenco@gmail.com>
2024-06-20 03:31:06 +0100

    chore: javascript module arrays

---
+++ javascript/aula8.js
@@ -0,0 +1,9 @@
+// forecha so esta disponivel dentro de arrays
+const numeros = [1,2,3,56,7,8,999,98,9]
+
+let total = 0;
+numeros.forEach(function(valor,indice, arrays){
+return total+valor;
+})
+
+console.log(total)
