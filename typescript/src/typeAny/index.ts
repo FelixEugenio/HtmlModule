019c260d50c3bb3399dd1f1484cf42eb491140bd
felix mavila <felixmacarenco@gmail.com>
2024-06-24 01:14:00 +0100

    chore: type annotations

--- typescript/src/typeAny/index.ts
+++ typescript/src/typeAny/index.ts
@@ -1,3 +1,25 @@
 function showMessage(msg:string){
   return msg;
 }
+
+function semRetorno(...args:string[]):void{
+  console.log(args.join(' '))
+}
+
+semRetorno('felix','Mavila');
+
+
+
+const pessoa = {
+  nome:'Luiz',
+  sobrenome:'mavila',
+
+  exibirNome():void{
+  console.log(this.nome + ' ' + this.sobrenome)
+  }
+}
+
+pessoa.exibirNome()
+
+export {pessoa}
+
