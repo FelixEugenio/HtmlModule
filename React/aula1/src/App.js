3efd4b689ed8ce41485d499374bed21e817a849f
felix mavila <felixmacarenco@gmail.com>
2024-06-25 21:41:20 +0100

    chore: setState

--- React/aula1/src/App.js
+++ React/aula1/src/App.js
@@ -10,8 +10,25 @@ function App() {
 import { Component } from "react";
 
 class App extends Component{
+
+  constructor(props){
+    super();
+    this.handlePclick = this.handlePclick.bind(this);
+    this.state = {
+      name:'felix mavila'
+    }
+
+  }
+
+  handlePclick(){
+    //const {name} = this.state;
+    //console.log(`h1 clicado ${name}`)
+    this.setState({name:'Eugenio'})
+  }
+
   render(){
-    return <h1>Ola</h1>
+    const name = this.state.name;
+    return <h1 onClick={()=>this.handlePclick()}>{name}</h1>
   }
 }
 
