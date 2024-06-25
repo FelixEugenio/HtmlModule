a588bc5c8f81b200f6638dd440d0c1f501c12017
felix mavila <felixmacarenco@gmail.com>
2024-06-25 21:18:48 +0100

    chore: class component statefull

--- React/aula1/src/App.js
+++ React/aula1/src/App.js
@@ -1,9 +1,18 @@
 
-
+/*
 function App() {
   return (
     <h1>ola mundo</h1>
   );
+}
+  */
+
+import { Component } from "react";
+
+class App extends Component{
+  render(){
+    return <h1>Ola</h1>
+  }
 }
 
 export default App;
