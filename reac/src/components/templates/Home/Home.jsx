71ef1f5ae38a470169dfa7f350589a94a643ffed
felix mavila <felixmacarenco@gmail.com>
2024-06-30 20:06:16 +0100

    refactor: move to correct folder

--- reac/src/components/templates/Home/Home.jsx
+++ reac/src/components/templates/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, useEffect, useState,useCallback } from "react";
 import './styles.css'
 
 import { loadPosts} from  '../../../utils/load-posts'
@@ -6,7 +6,104 @@ import { Posts } from '../../Posts/index';
 import { Button } from "../../Button";
 import { TextInput } from "../../Input";
 
- class Home extends Component {
+export  const Home = () =>{
+  /*
+  const {
+    page,
+    postsPerPage,
+    allPosts,
+    posts,
+    searchValue
+  } = this.state;
+   */
+
+  const [posts,setPosts] = useState([]);
+  const [allPosts,setAllPosts] = useState([]);
+  const [page,setPage] = useState(0);
+  const [searchValue,setSearchValue] = useState('');
+  const [postsPerPage,setPostsPerPage] = useState(10);
+
+  const handleLoadPosts = useCallback(async(page,postsPerPage) =>{
+
+   const postsAndPhotos = await loadPosts();
+   
+  setPosts(postsAndPhotos.slice(page,postsPerPage));
+  setAllPosts(postsAndPhotos)
+  },[])
+
+  const loadMorePosts = () =>{
+
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage,nextPage + postsPerPage)
+    posts.push(...nextPosts)
+
+    setPosts(posts);
+    setPage(nextPage);
+  }
+
+  const handleChange = (event) =>{
+    const {value} = event.target;
+  
+    setSearchValue(value)
+
+  }
+
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+
+  const filteredPosts = !!searchValue ? 
+    
+    allPosts.filter(post=>{
+      return post.title.toLowerCase().includes(searchValue.toLowerCase());
+    })
+    
+    : posts;
+
+    useEffect(()=>{
+    handleLoadPosts(0,postsPerPage)
+    },[handleLoadPosts,postsPerPage])
+
+  return (
+    <section className="container">
+      <div className="search-container">
+      {!!searchValue && (
+        <h1>search value: {searchValue}</h1>
+      )}
+       
+       <TextInput 
+       searchValue={searchValue}
+       handleChange={handleChange}
+       
+       />
+      </div>
+      
+
+      {filteredPosts.length > 0 && (
+      <Posts  posts={filteredPosts}/>
+      )}
+
+      {filteredPosts.length == 0 && (
+       <p>Nao existem Posts</p>
+      )}
+
+
+      <div className="button-container">
+        {!searchValue && (
+          <Button
+          disabled={noMorePosts}
+          onClick={loadMorePosts}
+           text = "Load more page"
+           />
+        )}
+      
+      </div>
+      
+    </section>
+  );
+} 
+
+/*
+
+ class Home2 extends Component {
 
   state = {
    posts: [],
@@ -53,64 +150,14 @@ import { TextInput } from "../../Input";
      
   render(){
 
-    const {
-      page,
-      postsPerPage,
-      allPosts,
-      posts,
-      searchValue
-    } = this.state;
-
-    const noMorePosts = page + postsPerPage >= allPosts.length;
-
-    const filteredPosts = !!searchValue ? 
     
-    allPosts.filter(post=>{
-      return post.title.toLowerCase().includes(searchValue.toLowerCase());
-    })
+
     
-    : posts;
 
-    return (
-      <section className="container">
-        <div className="search-container">
-        {!!searchValue && (
-          <h1>search value: {searchValue}</h1>
-        )}
-         
-         <TextInput 
-         searchValue={searchValue}
-         handleChange={this.handleChange}
-         
-         />
-        </div>
-        
-
-        {filteredPosts.length > 0 && (
-        <Posts  posts={filteredPosts}/>
-        )}
-
-        {filteredPosts.length == 0 && (
-         <p>Nao existem Posts</p>
-        )}
-
-
-        <div className="button-container">
-          {!searchValue && (
-            <Button
-            disabled={noMorePosts}
-            onClick={this.loadMorePosts}
-             text = "Load more page"
-             />
-          )}
-        
-        </div>
-        
-      </section>
-    );
+    
   }
   
 }
  
-
+*/
 export default Home;
