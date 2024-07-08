5e8b3c506fb0e01638eb51ec6a210c5a4b84cc52
felix mavila <felixmacarenco@gmail.com>
2024-07-08 19:22:39 +0100

    feat: add render type conf

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,22 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-const [name,setName] = useState('Felix');
-const [age,setAge] = useState(23);
-const [password,setPassword] = useState('Maravilha2020@');
-const [todos,setTodos] = useState('')
+const [counter,setCounter] = useState(0);
 
-function handleClick(){
-  setName('Mavila')
-  setAge(()=>
-     age + 3)
-  if(age===29){
-  return;
-  }
-}
+
+useEffect(()=>{
+
+},[])
   return (
-    <><h2>{age}</h2><h1>{name}</h1><button onClick={handleClick}>Click
-    </button></>
+    <div>
+      <h1> contador : {counter}</h1>
+      <button onClick={()=> setCounter(counter+1)}>+</button>
+    </div>
   );
 }
 
